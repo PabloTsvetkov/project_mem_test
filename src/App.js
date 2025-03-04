d88a5661cf5d11de824a00877768137625b19dea
PabloTsvetkov <pcvetkov53@gmail.com>
2025-03-04 12:51:21 +0300

    fc

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
+import { useState, useEffect, useRef, forwardRef } from 'react';
 import './App.css';
+import axios from 'axios';
+import { createChart, LineSeries } from 'lightweight-charts';
+
+const Chart = ({ chartData: chartData, isChart: isChart, setIsChart: setIsChart }) => {
+  const chartRef = useRef(null);
+
+  if (isChart) {
+    chartRef.current = null;
+  }
+
+  useEffect(() => {
+    if (!chartRef.current || !chartData) {
+      return;
+    }
+
+    const chart = createChart(chartRef.current, {
+      width: 600,
+      height: 600,
+      timeScale: {
+        timeVisible: true,
+        secondsVisible: true,
+      }
+    });
+
+    const lineSeries = chart.addSeries(LineSeries, {});
+
+    const sortedData = chartData.slice().sort((a, b) => a.time - b.time);
+    lineSeries.setData(sortedData);
+    chart.timeScale().fitContent();
+    setIsChart(true);
+  }, [chartData]);
+
+  return <div ref={chartRef}></div>
+
+}
 
 function App() {
+  const [chartData, setChartData] = useState();
+  const [isData, setIsData] = useState(false);
+  const [isChart, setIsChart] = useState(false);
+
+  const handleButtonClick = () => {
+    const params = new URLSearchParams({
+      category: 'linear',
+      symbol: 'BTCUSDT',
+      interval: '5',
+      limit: '40'
+
+    });
+    axios.get(`https://api-testnet.bybit.com/v5/market/kline?${params.toString()}`)
+      .then((response) => {
+        const list = response?.data?.result?.list;
+        const data = Object.values(list).map((item) => ({
+          time: Math.round(Number(item[0]) / 1000),
+          value: Number(item[3])
+        }));
+        setChartData(data);
+        setIsData(true);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handleButtonClick}>some button</button>
+      {!isData && !isChart ? <p>клик сам батон</p> : <Chart chartData={chartData} setIsChart={setIsChart} isChart={isChart}></Chart>}
     </div>
   );
 }
